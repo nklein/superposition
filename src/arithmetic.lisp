6ee305f5b2fa5091cae5a6c314bac13bb5c1eb36
Patrick Stein <pat@nklein.com>
2019-09-16 15:54:40 -0500

    Included ability to add numbers to gaussians

--- src/arithmetic.lisp
+++ src/arithmetic.lisp
@@ -11,11 +11,18 @@
 
 (defmethod binary-+ ((a number)
                      (b gaussian-random-variable))
-  (assert (zerop a))
-  b)
+  (if (zerop a)
+      b
+      (make-dependent-gaussian-random-variable (list b)
+                                               (cl:+ a (μ b))
+                                               (σ² b))))
+
+(defmethod binary-+ ((a gaussian-random-variable)
+                     (b number))
+  (binary-+ b a))
 
 (defmethod binary-+ ((a gaussian-random-variable)
                      (b gaussian-random-variable))
   (make-dependent-gaussian-random-variable (list a b)
-                                           (+ (μ a) (μ b))
-                                           (+ (σ² a) (σ² b))))
+                                           (cl:+ (μ a) (μ b))
+                                           (cl:+ (σ² a) (σ² b))))
